b0beccbe22918c0740f5ff8ac2f0151372f129db
Rentaro Mori <re_mori@yumemi.co.jp>
2024-05-15 16:22:11 +0900

    fix: domain

--- src/domain/repository/user.rs
+++ src/domain/repository/user.rs
@@ -1,9 +1,9 @@
 use async_trait::async_trait;
 
-use crate::domain::model::user::{ActiveModel, Model as UserModel};
+use crate::domain::model::user::Model as UserModel;
 use crate::domain::repository::error::user::Error as UserRepositoryError;
 
 #[async_trait]
-pub trait UserRepository {
+pub trait UserRepository: Sync + Send {
     async fn save(&self, user: UserModel) -> Result<(), UserRepositoryError>;
 }
